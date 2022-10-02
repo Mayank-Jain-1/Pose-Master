724141350e302cfd3c78993ed88d4ed7751d5c72
Mayank <kaushalmayank.k@gmail.com>
2022-10-02 18:13:26 +0530

    home page added

--- Game.py
+++ Game.py
@@ -4,6 +4,7 @@ import numpy as np
 import mediapipe as mp 
 from keras.models import load_model 
 import pygame, time
+import sys
 
 
 pygame.init()
@@ -65,6 +66,31 @@ class Button():
                 return True
       return False
 
+def home_page():
+  global page
+
+  start_time = time.time()
+  while True:
+    for event in pygame.event.get():
+          if event.type == pygame.QUIT:
+              pygame.quit()
+              break
+    window.blit(bg4, (0,0))
+    window.blit(pygame.transform.scale(title_with_man,(800,800)), (200,-150))
+
+    button_start = Button("#000000","#ffffff",270, 500, 300,100,70,marcellus, text="Start")
+    button_start.draw(window,"#ffffff")
+    button_quit = Button("#000000","#ffffff",670, 500, 300,100,70,marcellus, text="Quit")
+    button_quit.draw(window,"#ffffff")
+
+    if button_start.check_click():
+      page = "start"
+      break
+    if button_quit.check_click() and time.time() - start_time > 4:
+      pygame.display.quit()
+      pygame.quit()
+      sys.exit()
+
 def start_page():
 
   
@@ -97,12 +123,12 @@ def start_page():
   window.blit(title, (335,335))
   button_start = Button("#34eb5b","#000000",1030, 200, 200,70,50,marcellus, text="Start")
   button_start.draw(window,"#000000")
-  button_exit = Button((255,0,0),"#000000",1030, 300, 200,70,50,marcellus, text="Quit")
-  button_exit.draw(window, (0,0,0))
+  button_back = Button((255,0,0),"#000000",1030, 300, 200,70,50,marcellus, text="Back")
+  button_back.draw(window, (0,0,0))
   if button_start.check_click():
     page = "game"
-    print("the page was changed, the button was pressed")
-
+  if button_back.check_click():
+    page = "home"
 
   if res.pose_landmarks and inFrame(res.pose_landmarks.landmark):
       for i in res.pose_landmarks.landmark:
@@ -122,7 +148,7 @@ def game_page():
 
   start_time = time.time()
   current_time = time.time()
-  given_time = 2
+  given_time = 30
   total_time = start_time + given_time
   score = 0
   increment = 1
@@ -168,12 +194,11 @@ def game_page():
     button_quit = Button((255,0,0),"#000000",1030, 300, 200,70,50,marcellus, text="QUIT")
     button_quit.draw(window, (0,0,0))
     if button_quit.check_click():
-      page = "start"
       break
 
     if level_completed:
       previous_level_name = level_name
-      while(level_name == previous_level_name):
+      while(level_name == previous_level_name) and level_name == "kicking":
         level_name = label[random.randint(0, len(label) -1)]
         level_completed = False
 
@@ -210,7 +235,7 @@ def end_game(score, time_taken):
   global page
 
   start_time = time.time()
-  gotime = 0
+  gotime = 1.5
   while True:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
@@ -226,17 +251,17 @@ def end_game(score, time_taken):
       score_text = Text("#252945",100,300,50,silkscreen,f"Score: {score}").draw(window)
       time_taken_text = Text("#252945",600,300,50,silkscreen,f"Time Played: {int(time_taken)} s ").draw(window)
       high_score_text = Text("#252945",100,400,50,silkscreen,f"HighScore: {high_score}").draw(window)
-      time_per_move_text = Text("#252945",600,400,50,silkscreen,f"Time Per Move: {0 if score == 0 else time_taken/score} s").draw(window)
+      time_per_move_text = Text("#252945",600,400,50,silkscreen,f"Time / Move: {round(int(time_taken) if score == 0 else time_taken/(score + 1),2)} s").draw(window)
 
-      button_exit = Button("#e32938","#000000",620, 550, 430,100,60,marcellus, text="Exit")
+      button_exit = Button("#e32938","#000000",620, 550, 430,100,60,marcellus, text="Main Menu")
       button_exit.draw(window, (0,0,0))
       button_play_again = Button("#49e68a","#000000",220, 550, 430,100,60,marcellus, text="Play Again")
       button_play_again.draw(window,"#000000")
       if button_play_again.check_click():
-        page = "game"
+        page = "start"
         break
       if button_exit.check_click():
-        page = "start"
+        page = "home"
         break
       
 
@@ -283,19 +308,22 @@ bg2 = pygame.image.load('Background_Images\Background2.png').convert_alpha()
 bg2 = pygame.transform.scale(bg2,(width,height))
 bg3 = pygame.image.load('Background_Images\Background3.png').convert_alpha()
 bg3 = pygame.transform.scale(bg3,(width,height))
+bg4 = pygame.image.load('Background_Images\Background4.png').convert_alpha()
+bg4 = pygame.transform.scale(bg4,(width,height))
 notvisible = pygame.image.load(r'Images\not_visible.png').convert_alpha()
 notvisible = pygame.transform.scale(notvisible, (220,220))
 clockimg = pygame.image.load(r'Images\clock.png').convert_alpha()
 clockimg = pygame.transform.scale(clockimg, (70,70))
 title = pygame.image.load(r'Images\title.png').convert_alpha()
 title = pygame.transform.scale(title, (600,600))
+title_with_man = pygame.image.load(r'Images\title_with_man.png').convert_alpha()
 poseImages = {i: pygame.transform.scale(pygame.image.load(f"poseImages\{i}.png").convert_alpha(),(200,200)) for i in label}
 gameover = pygame.image.load("Images\gameover3.png").convert_alpha()
 
 
 
 #game variables
-page = "start"
+page = "home"
 high_score = 10
 
 #main loop
@@ -304,8 +332,10 @@ while True:
   for event in pygame.event.get():
       if event.type == pygame.QUIT:
           pygame.quit()
-          break
-  if page == "start":
+          sys.exit()
+  if page == "home":
+    home_page(time.time())
+  elif page == "start":
     start_page()
   elif page == "game":
     game_page()
